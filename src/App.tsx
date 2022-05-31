a311e3c8e0819303bf19272ff16c09e2df44d6ef
蕾塞 <leisai@xiaohongshu.com>
2022-05-31 19:28:36 +0800

    chore: 增加注视

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,57 @@ const BIG_DELAY = 2000 * 0.35;
 const DELAY_STEP = (2000 * 0.3) / (150 / STEP);
 
 function App() {
-  const [rotateDeg, setRotateDeg] = useState(0);
-  const [exporting, setExporting] = useState(false);
-  const [canvasList, setCanvasList] = useState<HTMLCanvasElement[]>([]);
-  const svgRef = useRef<SVGSVGElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [leftBoardText, setLeftBoardText] = useState("");
   const [rightBoardText, setRightBoardText] = useState("");
 
+  // 是否正在导出gif中
+  const [exporting, setExporting] = useState(false);
+  const svgRef = useRef<SVGSVGElement>(null);
+  // 存每一帧的数组
+  const [canvasList, setCanvasList] = useState<HTMLCanvasElement[]>([]);
+  // 记录每一帧的旋转角度
+  const [rotateDeg, setRotateDeg] = useState(0);
   useEffect(() => {
-    if (exporting && svgRef.current) {
-      toCanvas(svgRef.current as unknown as HTMLElement).then((canvas) => {
-        canvasList.push(canvas);
-        console.log(canvas);
-        if (rotateDeg === MAX_ROTATE_DEG) {
-          const gif = new GIF({
-            width: 460,
-            height: 400,
-            workerScript: gifWorkerUrl,
-          });
-          gif.addFrame(canvasList[0]);
-          for (let i = 0; i < canvasList.length; i++) {
-            gif.addFrame(canvasList[i], {
-              delay: i === 0 ? BIG_DELAY : DELAY_STEP,
-            });
-          }
-          gif.addFrame(canvasList[canvasList.length - 1], { delay: BIG_DELAY });
-          for (let i = 0; i < canvasList.length; i++) {
-            gif.addFrame(canvasList[canvasList.length - 1 - i], {
-              delay: i === 0 ? BIG_DELAY : DELAY_STEP,
-            });
-          }
-          gif.addFrame(canvasList[0], { delay: BIG_DELAY });
-          gif.on("finished", function (blob) {
-            setExporting(false);
-            setCanvasList([]);
-            setRotateDeg(0);
-            window.open(URL.createObjectURL(blob));
-          });
-          gif.render();
-        } else {
-          setRotateDeg(rotateDeg + STEP);
-        }
-      });
+    if (!exporting || !svgRef.current) {
+      return;
     }
+    toCanvas(svgRef.current as unknown as HTMLElement).then((canvas) => {
+      canvasList.push(canvas);
+      // 当到最后一个帧时，开始导出gif
+      if (rotateDeg === MAX_ROTATE_DEG) {
+        const gif = new GIF({
+          width: 460,
+          height: 400,
+          workerScript: gifWorkerUrl,
+        });
+
+        // 增加正序动画帧
+        gif.addFrame(canvasList[0]);
+        for (let i = 0; i < canvasList.length; i++) {
+          gif.addFrame(canvasList[i], {
+            // 第一帧停留得比较久
+            delay: i === 0 ? BIG_DELAY : DELAY_STEP,
+          });
+        }
+        // 增加逆序动画帧
+        gif.addFrame(canvasList[canvasList.length - 1], { delay: BIG_DELAY });
+        for (let i = 0; i < canvasList.length; i++) {
+          gif.addFrame(canvasList[canvasList.length - 1 - i], {
+            delay: i === 0 ? BIG_DELAY : DELAY_STEP,
+          });
+        }
+        gif.addFrame(canvasList[0], { delay: BIG_DELAY });
+        gif.on("finished", function (blob) {
+          setExporting(false);
+          setCanvasList([]);
+          setRotateDeg(0);
+          window.open(URL.createObjectURL(blob));
+        });
+        gif.render();
+      } else {
+        setRotateDeg(rotateDeg + STEP);
+      }
+    });
   }, [rotateDeg, exporting]);
 
   return (
